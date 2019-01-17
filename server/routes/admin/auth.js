16afa1c53e65028a585ac0d8e4417f44c8fb4434
Irshad Ansari <irshadjsr21@gmail.com>
2019-01-17 14:14:23 +0530

    Added Profile and Change Password for admin

--- server/routes/admin/auth.js
+++ server/routes/admin/auth.js
@@ -3,7 +3,24 @@ const router = express.Router();
 
 const authController = require('../../controllers/admin/auth');
 const validator = require('../../middlewares/validator');
+const authenticator = require('../../middlewares/authenticator');
 
 router.post('/login', validator.userLogin, authController.login);
+
+router.get('/profile', authenticator('admin'), authController.getProfile);
+
+router.put(
+  '/profile',
+  authenticator('admin'),
+  validator.adminProfile,
+  authController.putProfile
+);
+
+router.post(
+  '/change-password',
+  authenticator('admin'),
+  validator.changePassword,
+  authController.changePassword
+);
 
 module.exports = router;
